db9cfb03ebc42445ae02bbbe6d54ec417a0f0902
francisco.nobre <franciscodiegonobre@gmail.com>
2023-02-01 22:56:40 +0100

    ejercicio-5

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Welcome from "./components/Welcome";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Link to="/">Inicio</Link>
+      <Link to="/app">Ir a la app</Link>
+      <hr />
+      <Routes>
+        <Route exact path="/" element={<Welcome />} />
+        <Route exact path="/app" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
